22c33104bcd8d1f2b1f4a418fb2322e082942a44
Pourdad Panahi <pourdadp@gmail.com>
2026-07-27 20:08:11 +0330

    Implement password hashing in config.py
    Added a function to hash passwords using SHA-256.

--- logbookV2/config.py
+++ logbookV2/config.py
@@ -1,2 +1,9 @@
-# config.py
+import hashlib
+
+# تنظیمات امنیتی و پایه
 DB_NAME = "lab_logbook.db"
+
+
+def hash_password(password):
+  """هش کردن پسورد با استاندارد امنیتی مناسب"""
+  return hashlib.sha256(password.encode()).hexdigest()
